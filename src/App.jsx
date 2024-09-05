4710f78190003e0f0ec34d03ab7b51c916412a51
Sachin Kumar <sachin.kumar@iiitg.ac.in>
2024-09-05 19:18:07 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "sachin Kumar",
+  title: "Web Designer",
+  email: "sachin.kumar@iiitg.ac.in",
+  gitHub: "shinewithsachin",
+  instagram: "shinewith_sachin",
+  linkedIn: "sachin",
 };
 
 const primaryColor = "#4E567E";
